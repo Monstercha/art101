e75e3228b62de033e89cf5ab69d127b97ba1877c
Monstercha <yuanyu1318574262@gmail.com>
2024-04-23 19:38:06 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,54 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Orange_Chicken.js
+// Description: An implementation of cooking Orange Chicken.
+// Author: Eros Yuan
+// Email: <yyuan70@ucsc.edu>
+// Date: April 23, 2024
 
-// Constants
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Gather ingredients and tools
+// Required: chicken breast, oranges, ginger, garlic, cilantro,soy sauce, sugar, white vinegar, cornstarch, salt, pepper, cooking oil, a wok, knife, cutting board, bowls, spoon
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+// Prepare the chicken
+// Clean and cut chicken breast into small pieces
+// In a bowl, add a pinch of salt, some pepper, and a big spoonful of cornstarch
+// Toss chicken pieces in the bowl until evenly coated
 
-// let's get this party started
-main();
+// Make the batter
+// In another bowl, mix a sufficient amount of cornstarch with a bit of water to form a paste
+
+// Mix the orange sauce
+// In a small bowl, combine fresh orange juice, soy sauce, sugar, white vinegar, and a teaspoon of cornstarch
+// Stir together and set aside
+
+// Cook the chicken
+// Pour enough cooking oil into a wok and heat to about 340 degrees Fahrenheit
+// Dip chicken pieces in the batter and fry until golden brown
+
+// Handle errors if oil is not hot enough
+  // If oil temperature is less than 340 degrees Fahrenheit
+  // Print "Please heat the oil to the correct temperature"
+  // Increase heat and recheck temperature
+
+// Saute the seasonings
+// Keep some oil in the wok, add finely chopped garlic and ginger
+// Fry until fragrant
+
+// Add the orange sauce
+// Pour the orange sauce mixture into the wok
+// Cook on medium heat until the sauce thickens
+
+// Add the chicken
+// Toss the fried chicken pieces into the wok
+// Stir quickly to coat them evenly with the orange sauce
+
+// Serve
+// Dish out the orange chicken and sprinkle some cilantro on top
+
+// Enjoy
+// Print "Enjoy your delicious homemade Orange Chicken!"
+
+
+
+
+
