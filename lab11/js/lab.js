694bfcb505e2ba0b58fa2c8da33e76dc36c6b563
Monstercha <yuanyu1318574262@gmail.com>
2024-05-19 18:53:13 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,25 +1,14 @@
 /*
  * Author: Eros Yuan <yyuan70@ucsc.edu>
- * Created:   15 May  2024
+ * Created:   18 May  2024
  * License: Public Domain
  */
 
-$("mybuttom").click(
-function(){
-
-  console.log("click");
-  let color = $("#myinput").val();
-
-  console.log("color:" + color)
-
-  $("BODY").append("<div>" + color + "</div>");
-
-  CSS("color",color);
-
-
-
-
-
-
+function sortString(inputString) {
+  return inputString.split('').sort().join('');
 }
-);
+$("#submit").click(function(){
+const userName = $("#user-name").val();
+userNameSorted = sortString(userName);
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
