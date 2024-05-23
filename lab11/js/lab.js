b004a705885bfacce4594ab09be8e0c201b65904
Monstercha <yuanyu1318574262@gmail.com>
2024-05-23 13:13:34 -0700

    lab12

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,8 +7,22 @@
 function sortString(inputString) {
   return inputString.split('').sort().join('');
 }
-$("#submit").click(function(){
-const userName = $("#user-name").val();
-userNameSorted = sortString(userName);
-$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+// Click listener for the button
+$("#submit").click(function() {
+  // Get values from input fields
+  const userName = $("#user-name").val();
+  const additionalInput1 = $("#usernameone").val();
+  const additionalInput2 = $("#usernametwo").val();
+
+  // Sort the input strings
+  const userNameSorted = sortString(userName);
+  const input1Sorted = sortString(additionalInput1);
+  const input2Sorted = sortString(additionalInput2);
+
+  // Append new sorted names to the output div
+  // This ensures we do not replace existing content, but rather add to it
+  $("#output").append('<div class="text"><p>' + userNameSorted + '</p ></div>');
+  $("#output").append('<div class="text"><p>' + input1Sorted + '</p ></div>');
+  $("#output").append('<div class="text"><p>' + input2Sorted + '</p ></div>');
 });
